3357985a24c122bda3fa374daf45842b77416cb5
s-cortes <s.cortes@uniandes.edu.co>
2024-03-01 15:33:05 -0500

    feat(k8): scripts para despliegue

---
+++ create_k8_deployment.sh
@@ -0,0 +1,42 @@
+echo "################################################################################################"
+echo "                                  G19 - Kubernetes Deployment                                   "
+echo "################################################################################################"
+
+echo "\n\n[K8s] Deployment ###########################################################################"
+echo "\n[K8s] Select Action to Execute"
+
+items=( "Deploy" "Remove" )
+select item in "${items[@]}" Quit
+do
+  read -p "[K8s] Do you Wish to $item Configuration? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+  case $REPLY in
+    1)
+      echo "\n[K8s] Starting Deployment from '.deployment'"
+      # kubectl create secret generic tasks-key --from-file=key.json=./deployment/misw4301-g19-2eea198443c9.json
+      kubectl apply -f ./deployment
+      echo "\n[K8s] Finished Deployment from '.deployment'\n"
+      break;;
+
+    2)
+      echo "\n[K8s] Starting Remove Deployment"
+      kubectl delete ingress gateway-ingress-k8
+      kubectl delete all --all -n default
+      echo "\n[K8s] Finished Removing Deployment\n"
+      break;;
+
+    3)
+      echo "\n[K8s] Quitting process\n"
+      break;;
+
+    *)
+      echo "\n[K8s] Invalid Option. Exiting\n"
+      break;;
+
+  esac
+done
+
+echo "################################################################################################"
+echo "                                      Completed Deployment                                      "
+echo "################################################################################################"
+exit 0
