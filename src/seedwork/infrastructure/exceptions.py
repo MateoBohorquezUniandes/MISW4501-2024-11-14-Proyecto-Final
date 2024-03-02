b7f7eea6362483887a82f629fc53a3fac4b49335
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-03-01 19:22:41 -0500

    Revert "Integración Main Viernes"

--- src/seedwork/infrastructure/exceptions.py
+++ src/seedwork/infrastructure/exceptions.py
@@ -15,11 +15,3 @@ class MissingDatabaseCredentials(InfrastructureException):
 
     def __str__(self):
         return str(self.__message)
-
-
-class MissingJWTSecrets(InfrastructureException):
-    def __init__(self, message="Missing jwt secret configuration"):
-        self.__message = message
-
-    def __str__(self):
-        return str(self.__message)
