f717081b7bc4577ecaab7a1dd2c17749af45117a
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-03-01 19:37:53 -0500

    Revert "Merge branch 'JWT_Component' into main"
    This reverts commit 9566d69e84f5503dbd5afd581138c6f5a9c8e444, reversing
    changes made to 02ff58086c481f906e4621b1231b96bb31156aae.

--- src/jwt_api_auth/application/dtos.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from dataclasses import dataclass, field
-
-from seedwork.application.dtos import DTO
-
-@dataclass(frozen=True)
-class TokenDTO(DTO):
-    user: str
-    password: str
