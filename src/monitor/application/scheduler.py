0d268439500d57eecd97ee963bc397be3bf81b49
Melissa Castro <m.castros@uniandes.edu.co>
2024-03-02 15:58:27 -0500

    write results in file

--- src/monitor/application/scheduler.py
+++ src/monitor/application/scheduler.py
@@ -4,30 +4,35 @@ import os
 import logging
 from datetime import datetime, time
 
-logging.basicConfig(format='%(message)s')
+logging.basicConfig(format='%(message)s',level=logging.INFO)
 log = logging.getLogger(__name__)
+EXPERIMENTO_ID = os.environ.get("EXPERIMENTO_ID")
+output_file_path = f"./src/monitor/outputs/{EXPERIMENTO_ID}.csv"
 
 def check_healty_services():
     print(f'user service called at {datetime.now()}')
     USERS_BASE_URL = os.environ["USERS_BASE_URL"]
     url = USERS_BASE_URL + '/health'
     response = requests.request("GET", url)
-    if(response.status_code != 200):
-        log.info(f'todo bien')
-    else:
-        log.info(f'todo mal')
+    is_healthy = response.status_code == 200
+    write_to_output(f'{datetime.now()};users;{"healthy"if(is_healthy) else "unhealthy"}','a')
 
 
 # Create a scheduler
 s = timesched.Scheduler()
 def start_job():
-    log.info(f'Job called at {datetime.now()}')    
+    log.info(f'Job called at {datetime.now()}')  
+    #write_to_output("time;service name;result","w")  
     # Execute job() every t secs
-    t = os.environ["TIME_SCHEDULER_SECS"]
+    t = int(os.environ["TIME_SCHEDULER_SECS"])
     s.repeat(t, 0, check_healty_services)
     # Run scheduler
     s.run()
 
-
+def write_to_output(message, mode):
+    print(message)
+    output_file =  open(output_file_path, mode)
+    output_file.write(f"{message}\n")
+    #output_file.close()
 
 
