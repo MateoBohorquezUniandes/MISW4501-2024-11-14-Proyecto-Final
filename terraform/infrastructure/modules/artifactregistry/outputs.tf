91785bfb668657a0d3c63f735cafdd266115029b
s-cortes <s.cortes@uniandes.edu.co>
2024-04-04 19:44:18 -0500

    feat(terraform): modulo para artifact registry

---
+++ terraform/infrastructure/modules/artifactregistry/outputs.tf
@@ -0,0 +1,7 @@
+output "artifact_registry_name" {
+  value = google_artifact_registry_repository.artifact_registry.name
+}
+
+output "artifact_registry_id" {
+  value = google_artifact_registry_repository.artifact_registry.id
+}
