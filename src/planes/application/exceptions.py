fef372828de751d17e6625afe0733e44e17e5c5e
Helena Patarroyo <h.patarroyo@uniandes.edu.co>
2024-04-11 11:46:32 -0500

    feat(HU029): Testing Postman Collection Update

--- src/planes/application/exceptions.py
+++ src/planes/application/exceptions.py
@@ -14,3 +14,7 @@ class BadRequestError(APIError):
 class UserNotFoundError(APIError):
     def __init__(self, message=HTTPStatus.NOT_FOUND.phrase):
         super().__init__(HTTPStatus.NOT_FOUND.value, message, "user.not_found")
+
+class UserNotAsociatedError(APIError):
+    def __init__(self, message=HTTPStatus.UNPROCESSABLE_ENTITY.phrase):
+        super().__init__(HTTPStatus.UNPROCESSABLE_ENTITY.value, message, "user.not_asociated_to_plan")
--- src/planes/application/queries/get_planes_asociados_usuario.py
+++ src/planes/application/queries/get_planes_asociados_usuario.py
@@ -8,7 +8,7 @@ from planes.application.queries.base import PlanQueryBaseHandler
 from planes.domain.entities import UsuarioPlan
 from seedwork.application.queries import Query, QueryResult, execute_query
 from seedwork.presentation.exceptions import APIError
-from planes.application.exceptions import UserNotFoundError
+from planes.application.exceptions import UserNotFoundError, UserNotAsociatedError
 
 
 @dataclass(frozen=True)
@@ -34,6 +34,9 @@ class GetPlanesAsociadosUsuarioQueryHandler(PlanQueryBaseHandler):
 
         except Exception as e:
             traceback.print_exc()
+
+            if "relation \"usuario_plan\" does not exist" in str(e):
+                raise UserNotAssociatedError(message="user.not_asociated_to_plan", code="usuario_plan_asociado.get.error.internal")
             raise APIError(message=str(e), code="usuario_plan_asociado.get.error.internal")
 
 
