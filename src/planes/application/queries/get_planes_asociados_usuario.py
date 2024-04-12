2ac2df1f0253ff79187272a0cf6fda45f3ce3e33
Helena Patarroyo <h.patarroyo@uniandes.edu.co>
2024-04-11 20:38:40 -0500

    feat(HU029): Mapper added

--- src/planes/application/queries/get_planes_asociados_usuario.py
+++ src/planes/application/queries/get_planes_asociados_usuario.py
@@ -26,8 +26,14 @@ class GetPlanesAsociadosUsuarioQueryHandler(PlanQueryBaseHandler):
             usuario_planes_asociados = repository.get(
                 query.tipo_identificacion, query.identificacion
             )
+            print('******consulta planes de usuario**********')
+            print(usuario_planes_asociados)
 
-            return QueryResult(result=usuario_planes_asociados)
+            mapper = UsuarioPlanDTOEntityMapper()
+
+            usuario_planes_asociados_dto = self.planes_factory.create(usuario_planes_asociados, mapper)
+
+            return QueryResult(result=usuario_planes_asociados_dto)
 
         except NoResultFound:
             traceback.print_exc()
@@ -35,9 +41,6 @@ class GetPlanesAsociadosUsuarioQueryHandler(PlanQueryBaseHandler):
 
         except Exception as e:
             traceback.print_exc()
-
-            if "relation \"usuario_plan\" does not exist" in str(e):
-                raise UserNotAssociatedError(message="user.not_asociated_to_plan", code="usuario_plan_asociado.get.error.internal")
             raise APIError(message=str(e), code="usuario_plan_asociado.get.error.internal")
 
 
