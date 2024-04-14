e5eea8e1fbcb00449b854003e63a370a3072339b
Helena-Pat29 <h.patarroyo@uniandes.edu.co>
2024-04-14 10:57:22 -0500

    Feat/planesHU029 Listar planes asociados (#32)
    Co-authored-by: Santiago Cortés Fernández <25346635+s-cortes@users.noreply.github.com>

--- src/planes/application/exceptions.py
+++ src/planes/application/exceptions.py
@@ -10,3 +10,11 @@ class UnprocessableEntityError(APIError):
 class BadRequestError(APIError):
     def __init__(self, message=HTTPStatus.BAD_REQUEST.phrase, code=""):
         super().__init__(HTTPStatus.BAD_REQUEST.value, message, code)
+
+class UserNotFoundError(APIError):
+    def __init__(self, message=HTTPStatus.NOT_FOUND.phrase):
+        super().__init__(HTTPStatus.NOT_FOUND.value, message, "user.not_found")
+
+class UserNotAsociatedError(APIError):
+    def __init__(self, message=HTTPStatus.UNPROCESSABLE_ENTITY.phrase):
+        super().__init__(HTTPStatus.UNPROCESSABLE_ENTITY.value, message, "user.not_asociated_to_plan")
---
+++ src/planes/application/queries/get_planes_asociados_usuario.py
@@ -0,0 +1,47 @@
+import traceback
+from dataclasses import dataclass, field
+
+from sqlalchemy.exc import NoResultFound
+
+from planes.application.mappers import UsuarioPlanDTOEntityMapper
+from planes.application.queries.base import PlanQueryBaseHandler
+from planes.domain.entities import UsuarioPlan
+from seedwork.application.queries import Query, QueryResult, execute_query
+from seedwork.presentation.exceptions import APIError
+from planes.application.exceptions import UserNotFoundError, UserNotAsociatedError
+
+
+
+@dataclass(frozen=True)
+class GetPlanesAsociadosUsuario(Query):
+    tipo_identificacion: str = field(default_factory=str)
+    identificacion: str = field(default_factory=str)    
+
+
+class GetPlanesAsociadosUsuarioQueryHandler(PlanQueryBaseHandler):
+
+    def handle(self, query: GetPlanesAsociadosUsuario) -> QueryResult:
+        try:
+            repository = self.repository_factory.create(UsuarioPlan)
+            usuario_planes_asociados = repository.get(
+                query.tipo_identificacion, query.identificacion
+            )
+
+            mapper = UsuarioPlanDTOEntityMapper()
+
+            usuario_planes_asociados_dto = self.planes_factory.create(usuario_planes_asociados, mapper)
+
+            return QueryResult(result=usuario_planes_asociados_dto)
+
+        except NoResultFound:
+            traceback.print_exc()
+            raise UserNotFoundError()       
+
+        except Exception as e:
+            traceback.print_exc()
+            raise APIError(message=str(e), code="usuario_plan_asociado.get.error.internal")
+
+
+@execute_query.register(GetPlanesAsociadosUsuario)
+def execute_query_usuario_plan(query: GetPlanesAsociadosUsuario):
+    return GetPlanesAsociadosUsuarioQueryHandler().handle(query)
